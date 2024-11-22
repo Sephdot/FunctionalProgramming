ba222846b8e17096223fd161d71ccb3ce92067dd
Joseph Eaton <josepheaton04@outlook.com>
2024-11-22 11:24:03 +0000

    Complete Challenge 1-3

---
+++ FP/FP/Exercises001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FP
+{
+    internal class Exercises001
+    {
+        public static Func<int, int> AddOne = num => num + 1;
+
+        public static Func<int, int> SquareIt = num => num * num;
+
+        public static Func<int, int> AddTen = num => num + 10;
+
+        public static Predicate<string> GrammarCheck =
+            input =>
+            {
+                return Regex.IsMatch(input, @"^A.*!$");
+            };
+    }
+}
--- FP/FP/Program.cs
+++ FP/FP/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            List<int> numbers = new List<int> { 4, 15, 55, 78, 12 };
+
+            numbers.ForEach(x => Console.WriteLine(Exercises001.AddTen(x)));
+
+            List<string> words = new List<string> { "Amazing!", "apple!", "Awesome!", "Artichoke" };
+
+            words.ForEach(word => Console.WriteLine(Exercises001.GrammarCheck(word)));
         }
     }
 }
