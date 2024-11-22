93289dc3c979ec8f9bc26117533a69894eb8886e
Joseph Eaton <josepheaton04@outlook.com>
2024-11-22 14:37:18 +0000

    Complete another batch of tasks

---
+++ FP/FP/Exercises002.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP
+{
+    internal class Exercises002
+    {
+        public static Func<List<string>> GetCoolPeople = () =>
+        {
+            return new List<string>() {"rich.neat@boardgamer.com", "poppy.mcdonnell@irishdancer.com", "neil.hughes@walkingoncustard.com", "alice.yang@midfielder.com", "pippa.austin@musician.com" };
+        };
+        public static Action<string> GetUsername = username =>
+        {
+            Console.WriteLine(username.Split('@')[0]);
+        };
+
+        public static Action<List<string>> PrintCoolPeople = coolPeople =>
+        {
+            coolPeople.ForEach(Console.WriteLine);
+        };
+
+        public static Action<List<int>> SquaredNums = numbers =>
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] *= numbers[i];
+            }
+        };
+
+        public static Action<List<int>> PrintNums = numbers =>
+        {
+            numbers.ForEach(Console.WriteLine);
+        };
+
+        public static Action<List<int>> PrintSquaredNums = numbers =>
+        {
+            SquaredNums(numbers);
+            PrintNums(numbers);
+        };
+    }
+}
--- FP/FP/Program.cs
+++ FP/FP/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Console.WriteLine("Hello, World!");
             List<int> numbers = new List<int> { 4, 15, 55, 78, 12 };
             numbers.Select(x => Exercises001.AddTen(x)).ToList().ForEach(Console.WriteLine);
 
-            /*numbers.ForEach(x => Console.WriteLine(Exercises001.AddTen(x)));*/
+            numbers.ForEach(x => Console.WriteLine(Exercises001.AddTen(x)));
 
             List<string> words = new List<string> { "Amazing!", "apple!", "Awesome!", "Artichoke" };
 
@@ -17,8 +17,12 @@
             Console.WriteLine(Exercises001.SumIndices("start", "pale"));
             Console.WriteLine(Exercises001.CheckValidEmail("david@northcoders.co.uk"));
 
+            Exercises002.GetCoolPeople().ForEach(Exercises002.GetUsername);
 
+            Exercises002.PrintCoolPeople(Exercises002.GetCoolPeople());
 
+            List<int> numList = new List<int> { 5, 8, 3, 6, 67 };
+            Exercises002.PrintSquaredNums(numList);*/
         }
     }
 }
